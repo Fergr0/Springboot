78b1714d9f84d33dda639e7ce6dadadb2ee2ed92
Fergr0 <fergr01bbx@gmail.com>
2025-01-24 12:29:16 +0100

    ara si

---
+++ tutorials/src/main/java/com/example/tutorials/model/TutorialsVO.java
@@ -0,0 +1,19 @@
+package com.example.tutorials.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Builder
+@Document
+public class TutorialsVO {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private Boolean published;
+}
